7043d80b14be7b2afc40ac675c2060db53084d60
liuxiaocong <liu.xiaocong@puscene.com>
2017-09-08 18:25:36 +0800

    build(wechat): add wechat header and footer nav component on prod env

--- dist/client/containers/Wechat/Self.6feb935e.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([3],{442:function(e,t,n){try{(function(){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var e=function(e){return e&&e.__esModule?e:{default:e}}(n(12));n(454);t.default=function(){return e.default.createElement("div",{className:"self-wrapper"},"Self")}}).call(this)}finally{}},454:function(e,t){}});
---
+++ dist/client/containers/Wechat/Self.e8945267.js
@@ -0,0 +1,1 @@
+webpackJsonp([4],{445:function(e,t,n){try{(function(){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var e=function(e){return e&&e.__esModule?e:{default:e}}(n(12));n(459);t.default=function(){return e.default.createElement("div",{className:"self-wrapper"},"Self")}}).call(this)}finally{}},459:function(e,t){}});
