2f0bd5fdc23e2ae82544c95d660a1bb5db84d4c5
liuxiaocong <liu.xiaocong@puscene.com>
2017-09-11 14:44:03 +0800

    build(wechat): add header for all pages on prod env

--- dist/client/containers/Wechat/Contact.d3b4decf.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([6],{443:function(e,t,n){try{(function(){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var e=function(e){return e&&e.__esModule?e:{default:e}}(n(12));n(457);t.default=function(){return e.default.createElement("div",{className:"contact-wrapper"},"Contact")}}).call(this)}finally{}},457:function(e,t){}});
