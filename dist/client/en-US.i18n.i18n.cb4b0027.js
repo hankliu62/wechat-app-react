db2649b21f3a92e152edac9dd52110699b31baa0
liuxiaocong <liu.xiaocong@puscene.com>
2017-09-12 20:30:42 +0800

    build(wechat): complete contact page, 30 percent done on prod env

--- dist/client/en-US.i18n.i18n.cb4b0027.js
+++ dist/client/en-US.i18n.i18n.cb4b0027.js
@@ -1,1 +1,1 @@
-webpackJsonp([10],{440:function(n,e){n.exports={app:{homepage:{content:"Homepage Content"},login:{content:"Login Page Content"},signup:{content:"SignUp Page Content",title:"SIGN UP",newAccount:"Create Account",tip:"同意并接受",userAgreement:"《服务条款》"}}}}});
+webpackJsonp([10],{582:function(n,e){n.exports={app:{homepage:{content:"Homepage Content"},login:{content:"Login Page Content"},signup:{content:"SignUp Page Content",title:"SIGN UP",newAccount:"Create Account",tip:"同意并接受",userAgreement:"《服务条款》"}}}}});
