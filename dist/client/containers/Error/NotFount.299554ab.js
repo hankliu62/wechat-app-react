1bc454d709bb49a285245bdd98ddcfab03d5a82d
liuxiaocong <liu.xiaocong@puscene.com>
2017-09-26 13:25:09 +0800

    build(wechat): integrate api for wechat pages on prod env

--- dist/client/containers/Error/NotFount.299554ab.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([25],{648:function(e,t,l){try{(function(){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var e=function(e){return e&&e.__esModule?e:{default:e}}(l(10));t.default=function(t){return e.default.createElement("div",null,e.default.createElement("h3",null,"No match for ",e.default.createElement("code",null,t.location.pathname)))}}).call(this)}finally{}}});
