1a4b59647d9af4fb5cef867cf30073dae2e45c19
liuxiaocong <liu.xiaocong@puscene.com>
2017-10-11 09:26:59 +0800

    build(explore): add wechat shake page on prod env

--- dist/client/zh-CN.i18n.i18n.0d20da70.js
+++ dist/client/zh-CN.i18n.i18n.0d20da70.js
@@ -1,1 +1,1 @@
-webpackJsonp([28],{629:function(n,e){n.exports={app:{homepage:{content:"首页内容"},login:{content:"登陆页面内容"},signup:{content:"注册页面内容",title:"注册",newAccount:"创建新账号",tip:"同意并接受",userAgreement:"《服务条款》"}}}}});
+webpackJsonp([29],{629:function(n,e){n.exports={app:{homepage:{content:"首页内容"},login:{content:"登陆页面内容"},signup:{content:"注册页面内容",title:"注册",newAccount:"创建新账号",tip:"同意并接受",userAgreement:"《服务条款》"}}}}});
